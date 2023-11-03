91d7c2a72945a1a8f3673900ebe2cac6682dace6
satyasai69 <ch.satya.sai133@gmail.com>
2023-11-03 20:26:55 +0530

    assigned

--- func.sol
+++ func.sol
@@ -5,8 +5,15 @@ contract Function {
     function returnmany() public pure returns(uint, bool, uint){
         return(1, true, 3);
     }
-    // return value by name
+    // return value can be  name
     function returnbyname() public pure returns(uint x, bool b, uint y) {
       return (1, true, 3);
+    }
+    // Return values can be assigned to their name.
+    // In this case the return statement can be omitted.
+    function assigned() public pure returns (uint x, bool b, uint y) {
+        x = 1;
+        b = true;
+        y = 2;
     }
 }
