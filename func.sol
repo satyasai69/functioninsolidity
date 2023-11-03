4eced5e0ff33464d2446401fd972dc3f384153dd
satyasai69 <ch.satya.sai133@gmail.com>
2023-11-03 20:22:13 +0530

    return many type this function

--- func.sol
+++ func.sol
@@ -1,3 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-contract Function {}
+contract Function {
+    //function can return one or more times like muilt
+    function returnmany() public pure returns(uint, bool, uint){
+        return(1, true, 3);
+    }
+}
